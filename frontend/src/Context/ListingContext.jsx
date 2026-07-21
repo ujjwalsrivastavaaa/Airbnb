ff7b6c3a4bc85a8c4be2f43ac37ab7a41cedd99b
ujjwalsrivastavaaa <ujjwalismyname1234@gmail.com>
2026-07-21 20:28:08 +0530

    Add ListingPage1 component for listing display

--- frontend/src/Context/ListingContext.jsx
+++ frontend/src/Context/ListingContext.jsx
@@ -1,22 +1,62 @@
-import React, { createContext, useState } from "react";
-
+import React, { createContext, useContext, useState } from "react";
+import axios from 'axios'
+import { authDataContext } from "./AuthContext.jsx";
 export const listingDataContext=createContext()
 function ListingContext( {children} ){
   
 
  let [title,setTitle]=useState("")
- let[description,setDescription]=useState("")
- let[frontEndImage1,setFrontEndImage1]=useState(null)
- let[frontEndImage2,setFrontEndImage2]=useState(null)
- let[frontEndImage3,setFrontEndImage3]=useState(null)
- let[backEndImage1,setbackEndImage1]=useState(null)
- let[backEndImage2,setbackEndImage2]=useState(null)
- let[backEndImage3,setbackEndImage3]=useState(null)
+ let [description,setDescription]=useState("")
+ let [frontEndImage1,setFrontEndImage1]=useState(null)
+ let [frontEndImage2,setFrontEndImage2]=useState(null)
+ let [frontEndImage3,setFrontEndImage3]=useState(null)
+ let [backEndImage1,setbackEndImage1]=useState(null)
+ let [backEndImage2,setbackEndImage2]=useState(null)
+ let [backEndImage3,setbackEndImage3]=useState(null)
+ let [rent,setRent]=useState("")
  let [city,setCity]=useState("")
  let[landmark,setLandmark]=useState("")
  let[category,setCategory]=useState("")
+let {serverUrl}=useContext(authDataContext)
 
+
+
+const handleAddListing=async ()=>{
+  try{
+    let formData=new FormData()
+ formData.append("title",title)
+ formData.append("image1",backEndImage1);
+ formData.append("image2",backEndImage2);
+formData.append("image3",backEndImage3);
+formData.append("description",description);
+formData.append("city",city);
+formData.append("rent",rent);
+formData.append("landmark",landmark);
+formData.append("category",category);
+
+
+let result=await axios.post(serverUrl+"/api/listing/add",formData,{withCredentials:true})
+
+console.log(result)
+
+  }
+  catch(error){
+   console.log(error)
+  }
+}
     let value={
+   title,setTitle,
+   frontEndImage1,setFrontEndImage1,
+   frontEndImage2,setFrontEndImage2,
+   frontEndImage3,setFrontEndImage3,
+   backEndImage1,setbackEndImage1,
+   backEndImage2,setbackEndImage2,
+   backEndImage3,setbackEndImage3,
+   rent,setRent,
+   city,setCity,
+   landmark,setLandmark,
+   category,setCategory,
+   handleAddListing
 
   }
     return(
